675fecdd71af5fdccb104104e98a79d6fad5e464
DG <gd.dev@libertymail.net>
2018-05-19 16:08:01 +0200

    add parameter  to set number of threads in every functions where it can be usefull in the local ancestry sub-module

--- python-package/loter/locanc/local_ancestry.py
+++ python-package/loter/locanc/local_ancestry.py
@@ -92,7 +92,7 @@ def learn_Sknn(pop, A_in, H_in, weights, penalty=40, num_threads=10):
     H_pop = H_in
 
     l_res_mix = fixa_pip_AH_knn(G_pop,
-                                nb_iter=1, nbclust=len(A_in), penalty=penalty, 
+                                nb_iter=1, nbclust=len(A_in), penalty=penalty,
                                 num_threads=num_threads,
                                 weights=weights,
                                 A_in=A_in,
@@ -105,7 +105,7 @@ def learn_S_join(pop, A_in, penalty=40, small_penalty=0, num_threads=10):
     G_pop = pop["G"]
 
     l_res_mix = fixa_pip_A_join(G_pop,
-                                nb_iter=1, nbclust=len(A_in), penalty=penalty, 
+                                nb_iter=1, nbclust=len(A_in), penalty=penalty,
                                 A_in=A_in,
                                 small_penalty=small_penalty,
                                 num_threads=num_threads
@@ -136,9 +136,9 @@ def clusters_to_list_pop(S, l_k):
     for k, v in get_items(d): res[S==k] = v
     return res
 
-def locanc_g_knn(l_h, g_adm, penalty=40, small_penalty=0):
+def locanc_g_knn(l_h, g_adm, penalty=40, small_penalty=0, num_threads=10):
     A_in = np.ascontiguousarray(np.vstack(l_h))
-    S_adm, H = learn_S_join({"G": g_adm}, A_in, penalty, small_penalty)
+    S_adm, H = learn_S_join({"G": g_adm}, A_in, penalty, small_penalty, num_threads)
     result = clusters_to_list_pop(S_adm, [len(A) for A in l_h])
 
     return result, S_adm, H
@@ -166,12 +166,12 @@ def encode_haplo(H):
     H1, H2 = H[::2], H[1::2]
     return ((np.maximum(H1, H2) * (np.maximum(H1, H2) + 1)) / 2) + np.minimum(H1, H2)
 
-def loter_multiple_pops(l_H, h_adm, lambd):
+def loter_multiple_pops(l_H, h_adm, lambd, num_threads=10):
     res_loter, _= locanc_h_knn([h.astype(np.uint8) for h in l_H],
-                               h_adm.astype(np.uint8), lambd, 20)
+                               h_adm.astype(np.uint8), lambd, num_threads)
     return res_loter
 
-def boostrap_loter_multiple_pops(l_H, h_adm, lambd, counts, nbrun=20):
+def boostrap_loter_multiple_pops(l_H, h_adm, lambd, counts, nbrun=20, num_threads=10):
 
     def shuffle(H):
         n, m = H.shape
@@ -183,25 +183,30 @@ def boostrap_loter_multiple_pops(l_H, h_adm, lambd, counts, nbrun=20):
             counts = update_counts(counts,
                                    loter_multiple_pops(shuffled_H,
                                                        h_adm,
-                                                       lambd),
+                                                       lambd,
+                                                       num_threads),
                                    len(l_H)
             )
     else:
         counts = update_counts(counts,
                                loter_multiple_pops(l_H,
                                                    h_adm,
-                                                   lambd),
+                                                   lambd,
+                                                   num_threads),
                                len(l_H)
         )
 
     return counts
 
-def loter_local_ancestry(l_H, h_adm, range_lambda=np.arange(1.5, 5.5, 0.5), rate_vote=0.5, nb_bagging=20):
+def loter_local_ancestry(l_H, h_adm, range_lambda=np.arange(1.5, 5.5, 0.5),
+                         rate_vote=0.5, nb_bagging=20, num_threads=10):
     input_loter = (l_H, h_adm)
     n, m = h_adm.shape
     counts = np.zeros((len(l_H), n, m))
     for l in range_lambda:
-        res_boostrap = boostrap_loter_multiple_pops(*input_loter, lambd=l, counts=counts, nbrun=nb_bagging)
+        res_boostrap = boostrap_loter_multiple_pops(*input_loter, lambd=l,
+                                                    counts=counts, nbrun=nb_bagging,
+                                                    num_threads=num_threads)
     res_loter = mode(counts)
     r = vote_and_impute(res_loter, rate_vote)
     return r, res_loter
@@ -210,22 +215,24 @@ def diploid_sim(cluster_found, cluster_truth):
     (n,m) = cluster_found.shape
     return np.count_nonzero(cluster_found == cluster_truth) / float(n*m)
 
-def find_lambda(s_in, threshold = 0.90, min_lambda = 1, max_lambda = 500):
+def find_lambda(s_in, threshold = 0.90, min_lambda = 1,
+                max_lambda = 500, num_threads=10):
     n, m = s_in.shape
     if max_lambda - min_lambda <= 1:
         return locanc_g_knn([np.zeros((1,m)), np.ones((1,m))],
-                            s_in, min_lambda, min_lambda)
+                            s_in, min_lambda, min_lambda, num_threads)
     else:
         mean = (max_lambda - min_lambda) / 2 + min_lambda
         r_g, s_g, h_g = locanc_g_knn([np.zeros((1,m)), np.ones((1,m))],
-                                     s_in, mean, mean)
+                                     s_in, mean, mean, num_threads)
         sim = diploid_sim(r_g[::2] + r_g[1::2], s_in)
         if sim > threshold:
             return find_lambda(s_in, threshold, min_lambda = (max_lambda - min_lambda) / 2 + min_lambda,
-                       max_lambda = max_lambda)
+                               max_lambda = max_lambda, num_threads=num_threads)
         else:
             return find_lambda(s_in, threshold, min_lambda = min_lambda,
-                       max_lambda = max_lambda  - ((max_lambda - min_lambda) / 2))
+                               max_lambda = max_lambda  - ((max_lambda - min_lambda) / 2),
+                               num_threads=num_threads)
 
 def vote_and_impute(s, percent_threshold=0.5):
     def select_val(s, percent_threshold):
@@ -252,13 +259,15 @@ def vote_and_impute(s, percent_threshold=0.5):
 
     return res
 
-def loter_smooth(l_H, h_adm, range_lambda=np.arange(1.5, 5.5, 0.5), threshold=0.90, rate_vote=0.5, nb_bagging=20):
-    res_impute, res_raw = loter_local_ancestry(l_H, h_adm, range_lambda, rate_vote, nb_bagging)
+def loter_smooth(l_H, h_adm, range_lambda=np.arange(1.5, 5.5, 0.5),
+                 threshold=0.90, rate_vote=0.5, nb_bagging=20, num_threads=10):
+    res_impute, res_raw = loter_local_ancestry(l_H, h_adm, range_lambda,
+                                               rate_vote, nb_bagging, num_threads)
     result = np.copy(res_impute)
     result_hap = []
     for i in range(len(res_impute)):
         arr_input = np.ascontiguousarray(np.array([res_impute[i]])).astype(np.uint8)
-        r, _, _ = find_lambda(arr_input, threshold=threshold)
+        r, _, _ = find_lambda(arr_input, threshold=threshold, num_threads=num_threads)
         result_hap.append(r)
         result[i] = r[::2] + r[1::2]
 
