ab193b9c224a2532f7b3a4c6d618981e73e045a5
Ghislain Durif <ghislain.durif@inria.fr>
2018-09-05 16:15:58 +0200

    move src inside python directory and integrate compilation into setup.py file (Makefile is now obsolete but kept for legacy reason)

--- python-package/loter/find_lib.py
+++ python-package/loter/find_lib.py
@@ -1,18 +1,25 @@
 import ctypes as C
 import os
+import pkg_resources
+from glob import glob
 
 class LoterLibraryNotFound(Exception):
     """Error thrown when loter is not found"""
     pass
 
 def _get_lib():
-    dirpath = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-    libpath_possible = [os.path.join(dirpath, '../../lib/', 'libloter.so'),
-                        os.path.join(dirpath, '../', 'libloter.so')]
-    libpath = [path for path in libpath_possible if os.path.exists(path) and os.path.isfile(path)]
+    # dirpath = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
+    # libpath_possible = [os.path.join(dirpath, '../../lib/', 'libloter.so'),
+    #                     os.path.join(dirpath, '../', 'libloter.so')]
+    # libpath = [path for path in libpath_possible if os.path.exists(path) and os.path.isfile(path)]
+    # if not libpath:
+    #     raise LoterLibraryNotFound("Library not found in :\n" + '\n'.join(libpath_possible))
+
+    libpath = glob(pkg_resources.resource_filename('loter',
+                   os.path.join('_loter_wrap*.so')))
 
     if not libpath:
-        raise LoterLibraryNotFound("Library not found in :\n" + '\n'.join(libpath_possible))
+        raise LoterLibraryNotFound("Library `_loter_wrap.so` not found")
 
     selected_lib = libpath[0]
     lib = C.cdll.LoadLibrary(selected_lib)
--- python-package/setup.py
+++ python-package/setup.py
@@ -1,14 +1,66 @@
-from setuptools import setup, find_packages, Extension
+from setuptools import find_packages
+from distutils.core import setup, Extension
+import distutils.util
 import os
+import sys
+from glob import glob
 
+## path
 curr_path = os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-lib_path = os.path.join(curr_path, '../lib/', 'libloter.so')
+src_path = os.path.join(curr_path, 'src')
+
+## src files
+src = []
+pattern = '*.cpp'
+for subdir,_,_ in os.walk(src_path):
+    src.extend(glob(os.path.join(subdir, pattern)))
+
+## dependencies
+dep = []
+patterns = ['*.h', '*.hpp']
+for subdir,_,_ in os.walk(src_path):
+    for pattern in patterns:
+        dep.extend(glob(os.path.join(subdir, pattern)))
+
+
+## include and linking
+incs = []
+libdirs = []
+libs = []
+link_flags = []
+
+## compile flags
+cxx_flags = ['-Wall', '-fPIC', '-O3', '-msse2']
+
+if "--no_openmp" in sys.argv:
+    cxx_flags.append('-DDISABLE_OPENMP')
+    sys.argv.remove("--no_openmp")
+    libs = ['stdc++', 'blas', 'lapack']
+else:
+    cxx_flags.append('-fopenmp')
+    link_flags.append('-fopenmp')
+    libs += ['stdc++', 'blas', 'lapack', 'gomp']
+
+## C++ extension
+loter_wrap = Extension(
+    'loter._loter_wrap',
+    sources = src,
+    include_dirs = incs,
+    extra_compile_args = cxx_flags,
+    library_dirs = libdirs,
+    libraries = libs,
+    extra_link_args = link_flags,
+    language = 'c++',
+    depends = dep,
+)
+
 
 setup(
     name='loter',
     version='0.1',
     description="Optimization Package to phase haplotypes",
     author="Thomas Dias-Alves",
+    url = 'https://github.com/bcm-uga/Loter',
     setup_requires=['numpy'],
     install_requires=[
         "numpy",
@@ -16,6 +68,7 @@ setup(
         "scikit-learn",
         "scipy"
     ],
+    ext_modules = [loter_wrap,],
     packages=find_packages(),
-    data_files=[lib_path]
+    data_files=[]
 )
